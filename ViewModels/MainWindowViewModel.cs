358987ff868c7cfd0ddf7d3b68aa86426e48172f
DmitryDutov <52903124+DmitryDutov@users.noreply.github.com>
2020-09-30 01:36:18 +0500

    Описание главнй команды и добавление в xaml

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -5,6 +5,9 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using CV19.Infrastructure.Commands.Base;
 
 namespace CV19.ViewModels
 {
@@ -35,7 +38,6 @@ namespace CV19.ViewModels
             // Либо вообще сокращаем до одной строки:
             set => Set(ref _Title, value);
         }
-
         #endregion
 
         #region Status - Статус программы
@@ -49,5 +51,37 @@ namespace CV19.ViewModels
             set => Set(ref _Status, value);
         }
         #endregion
+
+        // описываем главную команду (наделяем её функционалом) - закрытие приложения
+        // нужен ctor и region с описаним комманд
+
+        #region Команды
+
+        #region CloseApplicationCommand
+
+        // создаём свойство с типом ICommand
+        public ICommand ClosrAppkicationCommand { get; }
+        // теперь создаём два метода для команды
+        private void OnClosrAppkicationCommandExecuted(object p)
+        {
+            // Само действие команды (То что команда делает)
+            Application.Current.Shutdown();
+        }
+
+        private bool CanClosrAppkicationCommandExecute(object p) => true;
+
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Команды
+            // создаём значения комманд
+            ClosrAppkicationCommand = new LambdaCommand(OnClosrAppkicationCommandExecuted, CanClosrAppkicationCommandExecute);
+            
+
+            #endregion    
+        }
     }
 }
